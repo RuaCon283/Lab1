db230bf2a10c9db071fb36d0eee07d8399cacd5c
RuaCon283 <167230978+RuaCon283@users.noreply.github.com>
2026-01-06 16:47:22 +0700

    Initial React project

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <div>
+       <h1>
+        Trương Văn Vũ
+       </h1>
+       <h2>
+        28/03/2003  
+       </h2>
+      </div>
+
+    </>
+  )
+}
+
+export default App
